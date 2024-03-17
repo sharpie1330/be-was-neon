5f9476f40c768d2bfed45dab4b806539a7970a75
곽태영 <amberaplin14@gmail.com>
2024-03-17 23:21:15 +0900

    [step2] HttpResponse에 responseLine 구하는 메서드 추가

--- src/main/java/webserver/response/HttpResponse.java
+++ src/main/java/webserver/response/HttpResponse.java
@@ -6,13 +6,13 @@ import java.util.List;
 import java.util.Map;
 
 public class HttpResponse {
-    private final String protocol;
+    private final String version;
     private final HttpStatusCode httpStatusCode;
     private final Map<String, List<String>> header;
     private final byte[] body;
 
-    public HttpResponse(String protocol, HttpStatusCode httpStatusCode, Map<String, List<String>> header, byte[] body) {
-        this.protocol = protocol;
+    public HttpResponse(String version, HttpStatusCode httpStatusCode, Map<String, List<String>> header, byte[] body) {
+        this.version = version;
         this.httpStatusCode = httpStatusCode;
         this.header = header;
         this.body = body;
@@ -23,12 +23,16 @@ public class HttpResponse {
         return new HttpResponse(protocol, httpStatusCode, header, body);
     }
 
-    public String getProtocol() {
-        return protocol;
+    public String getVersion() {
+        return version;
     }
 
     public HttpStatusCode getHttpStatusCode() {
         return httpStatusCode;
+    }
+
+    public String getResponseLine() {
+        return getVersion() + " " + getHttpStatusCode().getMessage() + "\r\n";
     }
 
     public Map<String, List<String>> getHeader() {
