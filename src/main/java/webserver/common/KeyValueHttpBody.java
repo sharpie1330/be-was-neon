9b207a964158cc01b2d1e1ecf346e7a2cfb6a0ab
곽태영 <amberaplin14@gmail.com>
2024-03-26 21:15:02 +0900

    [step5] KeyValueHttpBody 기본생성자 private로 막고 팩토리 메서드만 사용 가능하도록 수정

--- src/main/java/webserver/common/KeyValueHttpBody.java
+++ src/main/java/webserver/common/KeyValueHttpBody.java
@@ -10,7 +10,7 @@ import java.util.Set;
 public class KeyValueHttpBody {
     private final Map<String, String> body;
 
-    public KeyValueHttpBody() {
+    private KeyValueHttpBody() {
         this.body = new HashMap<>();
     }
 
